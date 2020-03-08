901a7234b38c09740876d62b2d03bc93c47dc9c1
NavTheRaj <navrajkhanal61@gmail.com>
2020-03-08 18:00:02 +0545

    Added a form and its action

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
-
+from django.views.generic.edit import CreateView
 from .models import Post
 
 
@@ -14,3 +14,7 @@ class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
+class BlogCreateView(CreateView):
+    model=Post
+    template_name='post_new.html'
+    fields='__all__'
